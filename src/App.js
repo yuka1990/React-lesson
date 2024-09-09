11f0e778465c7f7803a3d683c9e0d29928b3ee6a
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-09-09 07:09:26 +0000

    todo機能追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Todo from './todo'
 
 function App() {
   return (
@@ -7,17 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todo List
         </p>
-        <p>テスト</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App">
+          <Todo />
+        </div>
       </header>
     </div>
   );
